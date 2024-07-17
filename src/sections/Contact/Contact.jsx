6043dadd74a31842ebf2d0df933c311ecd85ea92
Mansoor-P <munaf6007@gmail.com>
2024-07-17 16:29:23 +0530

    contact section

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,36 +1,48 @@
 import styles from './ContactStyles.module.css';
 
-const Contact = () => {
+function Contact() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.container}>
-        <div className={styles.section}>
-          <h3>Let's Connect</h3>
-          <a href="mailto:munaf6007@gmail.com" className={styles.email}>munaf6007@gmail.com</a>
-          <div className={styles.socialIcons}>
-            <a href="https://www.linkedin.com/in/mansoor0731/" className={styles.icon}>Li</a>
-            <a href="https://github.com/Mansoor-P" className={styles.icon}>Git</a>
-            
-          </div>
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form action="">
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
         </div>
-        
-        <div className={styles.section}>
-          <h3>Contact Me</h3>
-          <form>
-            <input type="email"  placeholder="Enter Email address" className={styles.emailInput} />
-            <button type="submit"  className={styles.subscribeButton}>Mail Me</button>
-          </form>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
         </div>
-      </div>
-    <hr className={styles.hr}/>
-      <div className={styles.footerBottom}>
-      <p>
-        &copy; Copyright &copy; 2024 All Rights reserved | This portfolio made
-        by <span style={{color: "#0987f2"}} >Mansoor Pathikonda</span><br />
-      </p>
-      </div>
-    </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required></textarea>
+        </div>
+        <input className="hover btn" type="submit" value="Submit" />
+      </form>
+    </section>
   );
-};
+}
 
 export default Contact;
