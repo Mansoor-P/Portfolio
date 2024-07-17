bcfeeac37b31b921339b7098d9e11efc5be206fe
Mansoor-P <munaf6007@gmail.com>
2024-07-17 16:01:03 +0530

    Hero Section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,22 +1,23 @@
-import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/aboutr.png";
-
-import sun from "../../assets/sun.svg";
-import moon from "../../assets/moon.svg";
-
-import githubLight from "../../assets/github-light.svg";
-import githubDark from "../../assets/github-dark.svg";
-import linkedinLight from "../../assets/linkedin-light.svg";
-import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/cv.pdf";
-import { useTheme } from "../../common/ThemeContext";
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/heroImg.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
 
-  const themeIcon = theme === "light" ? sun : moon;
-  const githubIcon = theme === "light" ? githubLight : githubDark;
-  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -39,17 +40,21 @@ function Hero() {
           <br />
           Pathikonda
         </h1>
-        <h2>Full stack Developer</h2>
+        <h2>Fullstack Developer</h2>
         <span>
+          <a href="https://x.com/mansoor0731" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
           <a href="https://github.com/Mansoor-P" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://www.linkedin.com/in/mansoor0731/" target="_blank">
+          <a href="https://linkedin.com/mansoor0731" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
-          Passionate about developing modern web applications using React and Spring Boot, with a focus on delivering high-quality solutions for commercial businesses.
+          With a passion for developing modern React web apps for commercial
+          businesses.
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
